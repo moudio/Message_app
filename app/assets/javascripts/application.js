4af206eb38b9acded027fb814970f0c60774c9ad
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-19 18:30:00 +0000

    Add favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,14 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+
+  $('.ui.dropdown').dropdown()
+  ;
+})
