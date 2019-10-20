a3d373fbbbc1d61e01935a372588949ba89a71de
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-20 14:23:22 +0000

    Adding the real time functionality

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,6 +3,9 @@
 class ChatroomController < ApplicationController
 before_action :require_user
   def index
+    @message = Message.new
     @messages = Message.all
    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users
-  resources :messages
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
+
 end
