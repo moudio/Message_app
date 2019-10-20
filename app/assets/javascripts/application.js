127d671b8a822ce3a37f9340ac595684fb8121db
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-20 11:00:26 +0000

    Adding restrictions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,10 @@
 
 $(document).on('turbolinks:load', function(){
 
-  $('.ui.dropdown').dropdown()
-  ;
+  $('.ui.dropdown').dropdown();
+
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  })
+;
 })
